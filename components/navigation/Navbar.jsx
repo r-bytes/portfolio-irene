7b8c79ce7afb04cfe8ba785a6116228ec8135c22
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-12-26 13:44:22 +0100

    feat: navbar

--- components/navigation/Navbar.jsx
+++ components/navigation/Navbar.jsx
@@ -1,8 +1,14 @@
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className="bg-secondary text-secondary h-8">
+            <ul className="flex justify-center space-x-6 font-thin tracking-wide">
+                <li> home </li>
+                <li> portfolio </li>
+                <li> eigen werk </li>
+                <li> over mij </li>
+                <li> contact </li>
+            </ul>
+        </nav>
     )
 }
 export default Navbar
