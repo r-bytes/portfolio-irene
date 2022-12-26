e1999ccf290e0cdfda3a7250ffa238d5ec51f564
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-12-26 13:43:11 +0100

    feat: tailwind customizations

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ module.exports = {
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            backgroundColor: {
+                primary: "var(--color-bg-primary)",
+                secondary: "var(--color-bg-secondary)",
+                primaryAccent: "var(--color-bg-primary-accent)",
+                secondaryAccent: "var(--color-bg-secondary-accent)",
+                button: "var(--color-bg-button)",
+                card: "var(--color-bg-card)",
+            },
+            textColor: {
+                primary: "var(--color-text-primary)",
+                secondary: "var(--color-text-secondary)",
+                accent: "var(--color-text-accent)",
+                btnText: "var(--color-bg-secondary)",
+            },
+            borderColor: {
+                primary: "var(--color-bg-primary)",
+                secondary: "var(--color-bg-secondary)",
+                input: "var(--color-bg-input)",
+                accent: "var(--color-text-accent)",
+            },
+        },
     },
     plugins: [],
 };
