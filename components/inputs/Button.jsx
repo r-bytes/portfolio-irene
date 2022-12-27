6627171719515d95aa7cbda345f5a3b7edf18799
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-12-27 16:24:37 +0100

    feat: button input

---
+++ components/inputs/Button.jsx
@@ -0,0 +1,8 @@
+const Button = ({ text, href, primary}) => {
+    return (
+        <button className={primary ? "px-4 py-2 bg-button text-primary uppercase font-semibold text-xs tracking-wider" : "cursor-none px-4 py-2 border text-secondary uppercase font-extralight text-xs tracking-wider"} href={href}>
+            {text}
+        </button>
+    )
+}
+export default Button
