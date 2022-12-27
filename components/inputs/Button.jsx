a5f5f3a2ef608d369ff3d0b1578743ce88153612
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-12-27 16:30:59 +0100

    refactor: clean ups

--- components/inputs/Button.jsx
+++ components/inputs/Button.jsx
@@ -1,4 +1,4 @@
-const Button = ({ text, href, primary}) => {
+const Button = ({ text, href, primary }) => {
     return (
         <button className={primary ? "px-4 py-2 bg-button text-primary uppercase font-semibold text-xs tracking-wider" : "cursor-none px-4 py-2 border text-secondary uppercase font-extralight text-xs tracking-wider"} href={href}>
             {text}
