263fbec5f12537f9d48e2ce638bf90f241e5fcfe
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-01-02 13:28:42 +0100

    style: fix positioning

--- components/layout/Hero.jsx
+++ components/layout/Hero.jsx
@@ -9,20 +9,20 @@ const Hero = () => {
     ]
 
     return (
-        <div className="w-full relative">
+        <div className="w-full min-h-fit relative">
             <Image src={"/images/heroimageB.jpg"} alt="heroimageB.jpg" width={1000} height={1000} className="w-full h-44 object-cover " />
             <Image src={"/images/mijnlogo-white.png"} alt="heroimageB.jpg" width={1000} height={1000} className="w-full object-contain absolute h-24 top-[10%]" />
-            <h3 className="absolute top-[65%] left-[40%] text-center text-lg text-white tracking-wider font-light"> Illustrator & Striptekenaar </h3>
-            <span className="absolute top-[80%] left-[45%]">
+            <h3 className="w-72 absolute top-[60%] left-[50%] translate-x-[-50%] backdrop-blur-sm p-1 rounded text-center text-lg text-white tracking-wider font-light"> Illustrator & Striptekenaar </h3>
+            <span className="absolute top-[83%] left-[50%] translate-x-[-50%]">
                 {socialUrls.map(s => (
                     <SocialIcon
-                        style={{ height: 20, width: 20, marginRight: ".5em" }}
-                        key={s.id}
-                        bgColor="white"
-                        fgColor="black"
-                        url={s.url}
+                    style={{ height: 20, width: 20, marginRight: ".5em" }}
+                    key={s.id}
+                    bgColor="white"
+                    fgColor="black"
+                    url={s.url}
                     />
-                ))}
+                    ))}
             </span>
         </div>
     )
--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -17,7 +17,7 @@ const PortfolioPage = () => {
 
     return (
         <div className="flex flex-col justify-center items-center">
-            <div className="min-h-48 bg-secondaryAccent p-12 flex-col justify-center items-center text-center">
+            <div className="w-full min-h-48 bg-secondaryAccent p-12 flex-col justify-center items-center text-center">
                 <h1 className="text-4xl mb-4"> Portfolio </h1>
                 <p className="px-10"> Kitty kitty ears back wide eyed head nudges or stand with legs in litter box, but poop outside pounce on unsuspecting person or always ensure to lay down in such </p>
                 <p className="px-10"> a manner that tail can lightly brush human's nose, so sit in window and stare ooh, a bird, yum. </p>
