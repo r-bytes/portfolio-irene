3f115bd959de87c51c98a2f60967fc5e21c67d2d
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-05-02 01:37:01 +0200

    refactor: cleanup

--- components/layout/PageBanner.jsx
+++ components/layout/PageBanner.jsx
@@ -2,8 +2,6 @@ import React from "react"
 
 const PageBanner = ({ title, description }) => {
     return (
-
-
         <div className="min-h-48 w-full bg-secondaryAccent px-2 py-10 sm:p-12 flex-col justify-center items-center text-center">
             <h1 className="text-4xl mb-4"> {title} </h1>
             <p className="px-10 max-w-7xl mx-auto leading-7">
