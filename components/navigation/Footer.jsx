321c1efb1d809bdcb99c321d738884b4eb7df3fe
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-05-04 14:46:30 +0200

    fix: styling issue, link must be clickable

--- components/navigation/Footer.jsx
+++ components/navigation/Footer.jsx
@@ -27,6 +27,7 @@ const Footer = () => {
     ]
 
     return (
+        <>
         <div className={footerStyle}>
             <div className="md:w-1/3 my-10">
                 <Image
@@ -35,14 +36,14 @@ const Footer = () => {
                     alt="heroimageB.jpg"
                     width={150}
                     height={150}
-                />
+                    />
             </div>
-            <div className="md:w-2/3 mt-10 tracking-wider text-center md:text-left">
-                <h3 className="text-lg text-accent mb-4"> Beschikbaar voor: bedrijfs illustraties, editorial illustraties, strips, Character design en Artwork in opdracht.  </h3>
-                <div className="flex flex-col text-accent"> {/* Icon Container*/}
+            <div className="md:w-2/3 mt-10 tracking-wider text-center md:text-left justify-center items-center gap-4">
+                <h3 className="text-lg text-accent mb-10"> Beschikbaar voor: bedrijfs illustraties, editorial illustraties, strips, Character design en Artwork in opdracht.  </h3>
+                <div className="flex flex-col text-accent justify-center items-center md:justify-start md:items-start gap-4"> {/* Icon Container*/}
                     <span className="block">
                         <MdOutlineEmail className="inline-block mr-2" />
-                        <h4 className="text-white text-xs inline-block"> {process.env.NEXT_PUBLIC_EMAIL} </h4>
+                        <a href={`mailto:${process.env.NEXT_PUBLIC_EMAIL}`} className="hover:text-neutral-300 cursor-pointer text-white text-xs inline-block"> {process.env.NEXT_PUBLIC_EMAIL} </a>
                     </span>
                     <span className="block">
                         <BsFillTelephoneFill className="inline-block mr-2" />
@@ -52,12 +53,9 @@ const Footer = () => {
                         <IoIosBusiness className="text-lg inline-block mr-2"/>
                         <h4 className="text-white">KVK: {process.env.NEXT_PUBLIC_KVK}</h4>
                     </span>
-                    <span className="text-white text-xs text-center mt-8">
-                        Art by Irene Berbee
-                    </span>
                 </div>
             </div>
-            <div className="md:w-1/3 flex justify-center mt-10">
+            <div className="md:w-1/3 flex justify-center mt-14">
                 {socialUrls.map(s => (
                     <IconButton key={s.id} style={{ borderRadius: "0px", padding: "0px"}}>
                         <SocialIcon
@@ -67,7 +65,7 @@ const Footer = () => {
                             url={s.url}
                             target="_blank"
                             rel="noopener noreferrer"
-                        />
+                            />
                     </IconButton>
                                       
                 ))}
@@ -76,6 +74,12 @@ const Footer = () => {
                 </a>
             </div>
         </div>
+        <div className={footerStyle}>
+            <span className="text-white text-xs text-center w-full">
+                Art by Irene Berbee
+            </span>
+        </div>
+        </>
     )
 }
 export default Footer
