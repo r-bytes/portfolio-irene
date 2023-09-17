262d5c36e53a55e0688eef580a81b14d5b723a20
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-09-17 20:28:24 +0200

    fix: email submit

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -15,25 +15,25 @@ const ContactPage = ({ bannerContent }) => {
     const { reset } = useForm()
 
     const handleSave = (formData) => {
-        console.log({ formData })
+        console.log("email send", { formData })
 
         // emailjs
-        // .send(
-        //     process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID, 
-        //     process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
-        //     formData,
-        //     process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
-        // )
-        // .then(
-        //   (result) => {
-        //     console.log(result.text);
-        //   },
-        //   (error) => {
-        //     console.log(error.text);
-        //   }
-        // );
-        // reset();
-        // router.push('/success')
+        .send(
+            process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID, 
+            process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+            formData,
+            process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+        reset();
+        router.push('/success')
     }
 
 
