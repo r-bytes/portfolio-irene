41b790bf1ef7c15737fc006edd9baebbd454196f
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-09-19 14:40:47 +0200

    style: fix position

--- components/layout/Hero.jsx
+++ components/layout/Hero.jsx
@@ -23,7 +23,6 @@ const Hero = () => {
                 height={1000}
                 className="w-full h-[22rem] lg:h-[26rem] object-cover"
                 // onClick={() => navigateTo("/")}
-
             />
             <div>
                 <Image
@@ -37,7 +36,8 @@ const Hero = () => {
                         absolute h-32 top-[18.7%] lg:top-[20%] left-[50%] translate-x-[-50%]
                     "
                 />
-                <h3 className="
+                <h3
+                    className="
                         min-w-fit w-5/6 lg:w-2/3 max-w-4xl pt-4 lg:pb-10 p-1 pb-5
                         text-center text-lg text-white tracking-wider font-light rounded-b-md bg-black/40
                         absolute top-[55%] lg:top-[50.7%] left-[50%] translate-x-[-50%]
@@ -46,14 +46,18 @@ const Hero = () => {
                     Illustrator & Striptekenaar
                 </h3>
             </div>
-            <span className="absolute top-[81%] lg:top-[79%] left-[50%] translate-x-[-50%] p-0">
-                {socialUrls.map(s => (
+            <span className="absolute top-[81%] lg:top-[79%] left-[43%] sm:left-[47%] md:left-[48%] lg:left-[48.5%] translate-x-[-50%] p-0">
+                {socialUrls.map((s) => (
                     <IconButton
                         key={s.id}
-                        style={{ borderRadius: "0px", padding: "0px"}}
+                        style={{ borderRadius: "0px", padding: "0px" }}
                     >
                         <SocialIcon
-                            style={{ height: 40, width: 40, marginRight: ".5em" }}
+                            style={{
+                                height: 40,
+                                width: 40,
+                                marginRight: ".5em",
+                            }}
                             key={s.id}
                             bgColor={"white"}
                             // fgColor={"black"}
@@ -61,13 +65,18 @@ const Hero = () => {
                             rel="noopener noreferrer"
                             url={s.url}
                         />
-
                     </IconButton>
                 ))}
-                <a target="_blank" href="https://ireneberbee.myportfolio.com" rel="noopener noreferrer">
+                <a
+                    target="_blank"
+                    href="https://ireneberbee.myportfolio.com"
+                    rel="noopener noreferrer"
+                >
                     <AiFillBehanceCircle
                         stroke="black"
-                        onClick={() => navigateTo("https://ireneberbee.myportfolio.com")}
+                        onClick={() =>
+                            navigateTo("https://ireneberbee.myportfolio.com")
+                        }
                         color="white"
                         size={47}
                         className="cursor-pointer rounded-full absolute top-[-10%] left-[113%] translate-x-[-50%] lg:top-[-10%]"
@@ -75,6 +84,6 @@ const Hero = () => {
                 </a>
             </span>
         </div>
-    )
+    );
 }
 export default Hero
