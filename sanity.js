95d006212db54e9499a121fbc3ff2c1a68b76e29
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-03-12 11:43:26 +0100

    fix: use cdn

--- sanity.js
+++ sanity.js
@@ -8,7 +8,8 @@ const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     apiVersion: "2021-10-21",
-    useCdn: process.env.NODE_ENV === "production"
+    // useCdn: process.env.NODE_ENV === "production"
+    useCdn: true
 }
 
 // set up the client for fetching data in the getProps page functions
